cfbfe9b0a8e2fe98343423af57e8ad36b07f0a91
Isidoro <hazuer_icl@hotmail.com>
2020-02-13 13:12:40 -0600

    Publicación de Servicios
    Creación del servicio hola

---
+++ config/zoap.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    
+    // Service configurations.
+
+    'services'          => [
+        
+        'demo'              => [
+            'name'              => 'Demo',
+            'class'             => 'Viewflex\Zoap\Demo\DemoService',
+            'exceptions'        => [
+                'Exception'
+            ],
+            'types'             => [
+                'keyValue'          => 'Viewflex\Zoap\Demo\Types\KeyValue',
+                'product'           => 'Viewflex\Zoap\Demo\Types\Product'
+            ],
+            'strategy'          => 'ArrayOfTypeComplex',
+            'headers'           => [
+                'Cache-Control'     => 'no-cache, no-store'
+            ],
+            'options'           => []
+        ],
+
+        'hola'              => [
+            'name'              => 'Hola',
+            'class'             => 'Viewflex\Zoap\Hola\HolaService',
+            'exceptions'        => [
+                'Exception'
+            ],
+            'types'             => [
+                'keyValue'          => 'Viewflex\Zoap\Hola\Types\KeyValue',
+                'product'           => 'Viewflex\Zoap\Hola\Types\Product'
+            ],
+            'strategy'          => 'ArrayOfTypeComplex',
+            'headers'           => [
+                'Cache-Control'     => 'no-cache, no-store'
+            ],
+            'options'           => []
+        ]
+    ],
+
+    
+    // Log exception trace stack?
+
+    'logging'       => true,
+
+    
+    // Mock credentials for demo.
+
+    'mock'          => [
+        'user'              => 'test@test.com',
+        'password'          => 'tester',
+        'token'             => 'tGSGYv8al1Ce6Rui8oa4Kjo8ADhYvR9x8KFZOeEGWgU1iscF7N2tUnI3t9bX'
+    ],
+
+    
+];
