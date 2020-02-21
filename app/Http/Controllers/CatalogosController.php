3800be4034cd826bf537a3cda389659587b1bdc0
Isidoro Cornelio <hazuer_icl@hotmail.com>
2020-02-20 18:05:29 -0600

    Actulización de catalogo de genero

--- app/Http/Controllers/CatalogosController.php
+++ app/Http/Controllers/CatalogosController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Types\Catalogos\Custom\Genero;
 use App\Types\Catalogos\Custom\TipoDocumento;
-use App\Types\Catalogos\Response\GeneroResponse;
 use App\Types\Catalogos\Response\TipoDocumentoResponse;
 
 
@@ -20,26 +18,6 @@ class CatalogosController
     public function __construct()
     {
         $this->repository = \App::make('App\Repositories\CatalogosRepository');
-    }
-
-    /**
-     * Lista de géneros
-     *
-     * @return App\Types\Catalogos\Response\GeneroResponse
-     */
-    public function getAllGeneros()
-    {
-        try
-        {        
-            return new GeneroResponse(1, 'Éxito', $this->repository->getAllGeneros()
-                ->map(function ($row) {
-                    return new Genero($row);
-                }));
-        }
-        catch (\Exception $e)
-        {
-            return new GeneroResponse(100, $e->getMessage());
-        }
     }
 
     /**
--- app/Repositories/CatalogosRepository.php
+++ app/Repositories/CatalogosRepository.php
@@ -2,16 +2,11 @@
 
 namespace App\Repositories;
 
-use App\Models\CatGeneroModel;
 use App\Models\CatDocumentoModel;
 
 class CatalogosRepository
 {
-    public function getAllGeneros()
-    {
-        return CatGeneroModel::all();
-    }
-    
+
     public function getAllDocumentos()
     {
         return CatDocumentoModel::all();
